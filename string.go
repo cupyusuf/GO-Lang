aa78bfc5843e4985fec50ea929bd9e33f649289c
Yusuf Supriadi <50407076+cupyusuf@users.noreply.github.com>
2021-11-20 22:37:02 +0700

    first commit

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Yusuf")
+	fmt.Println("Supriadi")
+	fmt.Println("Yusuf Supriadi")
+
+	fmt.Println(len("Yusuf Supriadi"))
+	fmt.Println("Yusuf"[0])
+	fmt.Println("Supriadi"[1])
+}
