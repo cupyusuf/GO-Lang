82a6f2ac721ba3672267ce3b4786d978adec875f
Yusuf Supriadi <50407076+cupyusuf@users.noreply.github.com>
2022-02-20 16:40:21 +0700

    for

--- for.go
+++ for.go
@@ -3,9 +3,25 @@ package main
 import "fmt"
 
 func main() {
-
 	for counter := 1; counter <= 10; counter++ {
 		fmt.Println("Perulangan Ke", counter)
+	}
 
+	slice := []string{"Yusuf", "Supriadi", "Golang", "Salma", "Gita"}
+
+	for i := 0; i < len(slice); i++ {
+		fmt.Println(slice[i])
+	}
+
+	for i, value := range slice {
+		fmt.Println("Index", i, "=", value)
+	}
+
+	person := make(map[string]string)
+	person["name"] = "Yusuf"
+	person["title"] = "Programmer"
+
+	for key, value := range person {
+		fmt.Println(key, "=", value)
 	}
 }
