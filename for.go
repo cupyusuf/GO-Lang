11bab43ec7522f9e685d9239779b355c3a4a4748
Yusuf Supriadi <50407076+cupyusuf@users.noreply.github.com>
2022-02-20 16:33:26 +0700

    Yusuf
    baru

--- for.go
+++ for.go
@@ -3,10 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	counter := 1
 
-	for counter <= 10 {
+	for counter := 1; counter <= 10; counter++ {
 		fmt.Println("Perulangan Ke", counter)
-		counter++
+
 	}
 }
